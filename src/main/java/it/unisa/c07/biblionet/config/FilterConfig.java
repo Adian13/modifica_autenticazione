e1bdf7e928fcab746ba681fa537b0db1503191c4
LoganHX <cosimostabile95@gmail.com>
2023-03-17 14:38:48 +0100

    Aggiunta autenticazione def

---
+++ src/main/java/it/unisa/c07/biblionet/config/FilterConfig.java
@@ -0,0 +1,23 @@
+package it.unisa.c07.biblionet.config;
+
+import it.unisa.c07.biblionet.filter.JwtFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        FilterRegistrationBean filter= new FilterRegistrationBean();
+        filter.setFilter(new JwtFilter());
+
+//        provide endpoints which needs to be restricted.
+//        All Endpoints would be restricted if unspecified
+        filter.addUrlPatterns("/conferma-modifica-utente"); //todo distinguere gli endpoint restricted
+        //filter.addUrlPatterns("/registrazione/*"); //todo distinguere gli endpoint restricted
+
+        return filter;
+    }
+}
